c0cfd848920a98d877470680e593ea0f1beee9c0
Paul Schmidt <rookian@gmail.com>
2021-02-04 23:23:54 +0100

    replaced +1/-1 operations by increment/decrement operator

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,29 +1,29 @@
 package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
-
+    
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+                        items[i].quality--
                     }
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                    items[i].quality++
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality++
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality++
                             }
                         }
                     }
@@ -31,7 +31,7 @@ class GildedRose(var items: Array<Item>) {
             }
 
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+                items[i].sellIn--
             }
 
             if (items[i].sellIn < 0) {
@@ -39,20 +39,19 @@ class GildedRose(var items: Array<Item>) {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].quality > 0) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+                                items[i].quality--
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        items[i].quality = 0
                     }
                 } else {
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                        items[i].quality++
                     }
                 }
             }
         }
     }
-
 }
 
